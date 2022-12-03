5f01043cd66d67e96b2e1334d99df0c030ca2d53
FedericoMario <117830052+FedericoMario@users.noreply.github.com>
2022-12-03 10:43:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include "lib.h"
+
 using namespace std;
-int main() {
-    int num = 0;
-    cin >> num;
-        if (num==1) {
-            cout << "numero non primo" << endl;
-        } else if (numeri_primi(num, num/2)) {
-            cout << "numero primo" << endl;
-        } else {
-            cout << "numero non primo" << endl;
-        }
-  return 0;
+int main(){
+    int a=0;
+    cin>>a;
+    if (numprimi(a)==0) {
+        cout<<"numero primo"<<endl;
+    }
+    else {
+        cout<<"numero non primo"<<endl;
+    }
+    return 0;
 }
+
+
