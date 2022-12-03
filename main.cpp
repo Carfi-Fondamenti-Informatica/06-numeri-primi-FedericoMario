1dff7d6aa572f53ee4887eb3d624e16a0b9b088b
FedericoMario <117830052+FedericoMario@users.noreply.github.com>
2022-12-03 11:23:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,12 @@
 
 using namespace std;
 int main(){
-    int n=0;
-    cin>>n;
+    int n=0; cin>>n;
     int m=(n-1);
     if (primo(n,m)==true);{
-        cout<<"numero primo"<<endl;
+        cout<<"numero primo";
     } else if (primo(n,m)==false){
-        cout<<"numero non primo"<<endl;
+        cout<<"numero non primo";
     }
     return 0;
 }
