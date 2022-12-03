00d7222da351248f4494e7b184a4683cebb46757
FedericoMario <117830052+FedericoMario@users.noreply.github.com>
2022-12-03 09:20:23 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+#include "lib.h"
+bool numeroprimo(int x, int k){
+    if(x % k == 0){
+        if(x == k){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    numeroprimo(x, k + 1);
+}
